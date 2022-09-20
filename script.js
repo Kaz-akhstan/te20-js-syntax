80b3e5fa6fb7c42b7863b3def02e1a2d9f6ebd00
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-09-20 13:59:31 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const inputField1 = document.querySelector('#input-field-a');
+const inputField2 = document.querySelector('#input-field-b');
+const answerField = document.querySelector('#answer-field');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const a = inputField1.value;
+    const b = inputField2.value;
+    if(isNaN(a) || isNaN(b))
+    {
+        answerField.textContent = ("Not a Number");
+    }
+    else{
+        
+        let c = Math.sqrt(a*a + b*b);
+        answerField.textContent = c;
+    }
+});
